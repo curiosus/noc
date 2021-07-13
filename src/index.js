4b0049080c6b13284d2d86c358927a27fbaf22b9
curiosus <curiosus@gmail.com>
2021-07-13 08:41:28 -0600

    starting random walker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import P5 from 'p5';
 
+class Walker {
+	constructor(width, height, x, y) {
+		this.width = width;
+		this.height = height;
+		this.x = x;
+		this.y = y;
+	}
+}
+
 const s = (sk) => {
-	sk.createCanvas(1024, 768);
-	sk.background(0, 0, 0);
-	p5.fill(255, 0, 0);
-	p5.ellipse(0, 0, 4, 4);
+
+	sk.setup = () => {
+		sk.createCanvas(1024, 768);
+		sk.background(0, 0, 0);
+		sk.fill(255, 0, 0);
+		sk.ellipse(0, 0, 4, 4);
+		
+
+	}
 }
 
 const p5 = new P5(s);
